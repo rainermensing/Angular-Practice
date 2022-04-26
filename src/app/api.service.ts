1e76c8fa7cf4af631e3af57704b27e1f29ff14ed
Rainer <rainer.mensing@hotmail.de>
2022-04-26 09:55:20 +0200

    small fixes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
-import { ICommerce } from 'src/app/commerce/models/commerce';
+import { ICommerce } from './commerce/models/commerce';
 
 
 @Injectable({
--- src/app/newsapi/newsapi.component.ts
+++ src/app/newsapi/newsapi.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsApiService } from '../api.service';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 
 @Component({
